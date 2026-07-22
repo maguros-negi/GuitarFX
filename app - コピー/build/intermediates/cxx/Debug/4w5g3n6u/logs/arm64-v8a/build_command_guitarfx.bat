38d27eb8fc59226d21fd5d22e86464dc1775e319
magros-negi <soutamirin@gmail.com>
2026-07-23 01:16:07 +0900

    File Adjusted

--- app - コピー/build/intermediates/cxx/Debug/4w5g3n6u/logs/arm64-v8a/build_command_guitarfx.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\souta\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\souta\\Downloads\\GuitarFX_v0.1\\GuitarFX_v0.1\\app\\.cxx\\Debug\\4w5g3n6u\\arm64-v8a" ^
-  guitarfx
